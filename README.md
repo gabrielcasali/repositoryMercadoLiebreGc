ceb7342ffc52450fa77a0f5c9ca34cc54fbd2781
gabrielcasali <gabriel9casali@gmail.com>
2023-11-27 17:13:35 -0300

    nuevos html

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # repositoryMercadoLiebreGc
-# repositoryMercadoLiebreGc
--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const app = express();
 
 app.use(express.static('public'));
 // Levantando el servidor en el puerto 3000
-app.listen(3000, ()=>{
-    console.log("servidor corriendo en puerto 3000")
+app.listen(3001, ()=>{
+    console.log("servidor corriendo en puerto 3001")
 });
 app.get('/',(req,res) => {
     res.sendfile((require('path')).resolve('./views/home.html'))
 });
+app.get('/register',(req, res)=>{
+    res.sendFile(__dirname + '/views/register.html')
+});
+app.get('/login',(req, res) =>{
+    res.sendFile(__dirname+'/views/login.html')
+});
+app.post('')
