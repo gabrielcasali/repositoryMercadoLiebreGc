a6ec6ce3299db740f300d77dc41523be1e06380f
gabrielcasali <gabriel9casali@gmail.com>
2023-11-27 22:54:32 -0300

    gabic9

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('public'));
-const fs = require('fs');
+//const fs = require('fs');
 const path = require('path');
 // Cargar el contenido de package.json
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+//const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res)=>{
     res.sendfile(__dirname + './views/home.html');
-});
+})
 
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + './views/login.html');
-});
+})
 
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + './views/register.html');
-});
+})
 
 app.listen(PORT, ()=>{
     console.log(`Servidor funcionando en el puerto ${PORT}`);
-});
+})
