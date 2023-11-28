7b6965b8025b94b1176699eef92c51819aa01c6a
gabrielcasali <gabriel9casali@gmail.com>
2023-11-27 22:02:31 -0300

    abcde

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 
 
-app.get('/', (req,res)=>{
+app.get('/home', (req,res)=>{
     res.sendFile(__dirname + 'views\home.html');
 });
 
